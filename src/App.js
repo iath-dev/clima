7fae641e8bb458ab0c58f4271f210942cbd842a5
iath-dev <developer.iath@gmail.com>
2020-02-05 08:57:48 -0500

    113. Creando el Formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Header } from './components';
+import { Form } from './containers';
 
 function App() {
   return (
     <React.Fragment>
       <Header title="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
