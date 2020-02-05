1ae7dcfe24cb1b6ad9bda22b6981b558b4a3948c
iath-dev <developer.iath@gmail.com>
2020-02-05 09:36:31 -0500

    115. Validando el Formulario

--- src/containers/form/index.jsx
+++ src/containers/form/index.jsx
@@ -4,6 +4,7 @@ import { formDefault } from '../../constants/docs';
 const Form = () => {
 
     const [data, setData] = React.useState(formDefault);
+    const [error, setError] = React.useState(false);
     const { city, country } = data;
 
     const handleChanges = (event) => {
@@ -11,8 +12,21 @@ const Form = () => {
         setData({ ...data, [name]: value });
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (city.trim() === '' || country.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+    }
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
+            { error && <p className="red darken-4 error">Todos los campos deben de ser diligenciados</p>  }
             <div className="input-field col s12">
                 <input
                     type="text"
@@ -40,6 +54,13 @@ const Form = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="country">Pais:</label>
+            </div>
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
      );
